2af07a942255d81f3b0aa45949255798d431b8b0
nicolasnna <nicolas.norambuenaa@gmail.com>
2024-05-19 18:39:38 -0400

    Add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 var morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('post', function (tokens, req, res) { 
   if (tokens.method(req,res) === 'POST') {
