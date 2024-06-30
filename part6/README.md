f31d25c904da7f3f12de87827aad91e3fcb70b48
nicolasnna <nicolas.norambuenaa@gmail.com>
2024-06-30 19:06:44 -0400

    feat: :sparkles: Add part6 Exercises 6.1 - 6.8

---
+++ part6/README.md
@@ -0,0 +1,10 @@
+# Exercises part6 Full-Stack-Open
+
+In this folder, I upload the answers of exercises from part 6, `Advanced state management`.
+
+Exercises:
+
+- 6.1 - 6.2: unicafe 
+- 6.3 - 6.8 : anecdotes
+
+Source: https://fullstackopen.com/en/part6
---
+++ part6/anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,25 @@
+import { useDispatch } from 'react-redux'
+import { newAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+  
+  const addAnecdote = (event) => { 
+    event.preventDefault()
+    const anecdote = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch(newAnecdote(anecdote))
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div><input name="anecdote" /></div>
+        <button type='submit'>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
